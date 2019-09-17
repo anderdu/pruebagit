9533dcbe85e98ff148b052303daa2c1ed927ef79
anderdu <aduran022@ikale.ehu.eus>
2019-09-17 12:16:59 +0200

    perro creado

---
+++ src/Perro.java
@@ -0,0 +1,8 @@
+public class Perro {
+    public Perro(){
+
+    }
+    public void ladra(){
+        System.out.println("guau");
+    }
+}
--- src/sout.java
+++ src/sout.java
@@ -2,5 +2,7 @@ public class sout {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Perro zuri = new Perro();
+        zuri.ladra();
     }
 }
